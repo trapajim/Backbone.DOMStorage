5cef0ea73b1a94c62e50db67c1e23d810a25b317
trapajim <tobsus@tobias-mac.local>
2016-04-15 16:28:04 +0900

    bugfix

--- Backbone.localPersist.js
+++ Backbone.localPersist.js
@@ -190,6 +190,7 @@
   // Override 'Backbone.sync' to default to localSync,
   // the original 'Backbone.sync' is still available in 'Backbone.ajaxSync'
   Backbone.sync = function(method, model, options, error) {
-    return Backbone.getSyncMethod(model).apply(this, [method, model, options, error]);
+    return Backbone.getSyncMethod(model,[method, model, options, error]);
   };
+  return Backbone.localPersist;
 }));
